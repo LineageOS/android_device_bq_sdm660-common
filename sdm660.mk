1d6d405a7f84c44d77ddd5110feeea254da7490a
Brinly T <brinly@brinly.me>
2018-10-27 23:04:59 +0200

    sdm660-common: Add IMS properties
    Change-Id: Iec3bd1fe6b0358659fe98f1b24c6723e719635f3

--- sdm660.mk
+++ sdm660.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.manager@1.0-java
 
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
