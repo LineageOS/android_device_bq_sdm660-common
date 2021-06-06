74d920de012458e6bf0ae0885c39303fba383d98
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2021-06-06 19:24:30 +0200

    sdm660-common: Build soundtrigger HIDL HAL
    Change-Id: I87e8a45cd0ab1ed52bdea097c13c8e33ae3eb5e4

--- sdm660.mk
+++ sdm660.mk
@@ -58,6 +58,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio.primary.sdm660 \
     audio.r_submix.default \
