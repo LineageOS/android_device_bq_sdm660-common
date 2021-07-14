ab32a1dd5b761e8ea84cc6faa0e40a1f0849a092
Bruno Martins <bgcngm@gmail.com>
2021-07-14 15:32:31 +0200

    sdm660-common: Define Soong namespaces
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ifd3d0f2ec016465f6e9989b39a6836cc88925559

--- sdm660.mk
+++ sdm660.mk
@@ -354,6 +354,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    device/bq/sdm660-common
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
