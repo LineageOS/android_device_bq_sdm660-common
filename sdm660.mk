1b19986aedc0f15fbf85abba35bd9d00c848f124
codeworkx <daniel.hillenbrand@codeworkx.de>
2021-07-14 15:39:31 +0200

    sdm660-common: Build binderized fingerprint HAL
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Icfd2d8e928781719d96d310962de416289e7595c

--- sdm660.mk
+++ sdm660.mk
@@ -176,7 +176,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \
