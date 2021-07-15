cc1b0536696fe2110f237382da71f7b92d35750f
derfelot <derfelot.git@gmail.com>
2021-07-15 09:18:14 +0200

    sdm660-common: Build android.hardware.radio@1.1
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: Ie06d2e8b1112cec4b611b7a6fbe4c3c39d3333ab

--- sdm660.mk
+++ sdm660.mk
@@ -332,6 +332,7 @@ PRODUCT_PACKAGES += \
 # Radio
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
     android.hardware.radio@1.2 \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
