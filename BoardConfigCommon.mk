38dce05c17777a19159fe00f97c75a5f9725b1ed
LuK1337 <priv.luk@gmail.com>
2021-07-15 11:41:33 +0200

    sdm660-common: Update qcom sepolicy include
    - CAF also moved away from Android.mk but choosed to name the
    makefile as "SEPolicy.mk" which we were naming as "sepolicy.mk"
    in LineageOS
    
    Ref:
    [0]: LineageOS/android_device_qcom_sepolicy@1ca2d27
    
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I073484877a27e496175664bb136a8412051ae77c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,7 +184,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/firmware_mnt:/firmware
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
