de1e0f2c1819d758cfe85a0b637f44c099631d77
Weilun Du <wdu@google.com>
2022-06-30 02:01:23 +0200

    sdm660-common: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- sdm660.mk
+++ sdm660.mk
@@ -455,6 +455,7 @@ PRODUCT_PACKAGES += \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
     libwpa_client \
+    TetheringConfigOverlay \
     WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
