50051a99968b2e2896a8de153a3ede74b3f37700
Aayush Gupta <aayushgupta219@gmail.com>
2022-06-30 11:24:33 +0200

    sdm660-common: Unpin & patch libdpmframework.so using patchelf from tools-lineage
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ic5f6c44f7b17677bb392ab560972a7e1d12c6620

--- extract-files.sh
+++ extract-files.sh
@@ -49,6 +49,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        product/lib64/libdpmframework.so)
+            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper for common device
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
 
