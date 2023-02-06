da64bf0872488840f7c9abcdf78de0d20baab526
Alexander Koskovich <akoskovich@pm.me>
2023-02-06 19:00:13 +0100

    sdm660-common: Drop support for Snapdragon Camera.
     * Won't be implemented this cycle (or ever again hopefully).
    
    Change-Id: Ie2a346319dfe814deaf9833cb31640505cc6b7dc

--- sdm660.mk
+++ sdm660.mk
@@ -134,8 +134,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
-    Snap
+    libxml2
 
 # CABL
 PRODUCT_COPY_FILES += \
