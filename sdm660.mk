1f00b4755658148b36f530c9cdaf5c55dff67a94
Bruno Martins <bgcngm@gmail.com>
2023-03-11 20:44:53 +0100

    sdm660-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- sdm660.mk
+++ sdm660.mk
@@ -155,10 +155,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.1-service.clearkey \
-    android.hardware.drm@1.2-service.clearkey \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm-service.clearkey \
     android.hardware.drm@1.3.vendor \
     move_widevine_data.sh
 
