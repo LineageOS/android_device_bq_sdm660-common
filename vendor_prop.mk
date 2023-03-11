d28025954a2bc27d1a10ea73cfe2e18024c76ee3
=?UTF-8?q?Timi=20Rautam=C3=A4ki?= <timi.rautamaki@gmail.com>
2023-03-11 20:56:15 +0100

    sdm660-common: switch to threaded Skia render engine backend
    ´threaded´ option has issues with night light, etc.
    
    Co-authored-by: Michal Pražák <michal.prazak1@gmail.com>
    Change-Id: I142b95a38bf664f54c12d5c17ab11214375f705b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -106,7 +106,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.enable_fb_ubwc=1 \
-    debug.renderengine.backend=threaded \
+    debug.renderengine.backend=skiaglthreaded \
     debug.sf.hw=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.latch_unsignaled=1 \
