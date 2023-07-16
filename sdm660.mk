1a2886d5bffc6a710e37253410e86d9a47b4bed0
Mathieu Chartier <mathieuc@google.com>
2023-07-16 18:41:37 +0200

    sdm660-common: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- sdm660.mk
+++ sdm660.mk
@@ -414,6 +414,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/bq/sdm660-common
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
